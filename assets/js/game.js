7bb170e6c07ee0edb31d3603202920a483b730b6
Wilmer Martinez <wmartinez.mail@gmail.com>
2022-07-20 18:35:46 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,15 +67,44 @@ var fight = function(enemyName) {
 }; // end of fight function
 
 
+var startGame = function() {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 
-for (var i = 0; i < enemyNames.length; i++) {
-  if (playerHealth > 0) {
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
     window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
-  } else {
+    } else {
       window.alert("You have lost your robot in battle! Game Over!");
       break;
-  }
+    }
   var pickedEnemyName = enemyNames[i];
   enemyHealth = 50;
   fight(pickedEnemyName);
+  }
+
+  endGame();
 }
+
+// function to end the entire game
+var endGame = function() {
+   // if player is still alive, player wins!
+   if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } else {
+    window.alert("You've lost your robot in battle.");
+  }
+  // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+  // restart the game
+    startGame();
+  } else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  } 
+};
+ 
+startGame();
